cfb974b053eeb95398ec04986a55343f04354255
mtsukuda <mats.tsukuda@gmail.com>
2021-01-19 11:53:00 +0900

    create functions.

---
+++ src/orders/detail.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/**
+ * packages
+ */
+const fetch = require('node-fetch');
+
+/**
+ * @param event
+ * @returns {Promise<{body: string, statusCode: number}>}
+ */
+module.exports.func = async event => {
+  console.log(event);
+  console.log(event.query);
+  if (event.query === undefined) {
+    console.error("Could not find query.");
+    return {
+      statusCode: 400,
+      body: JSON.stringify('400 Bad Request'),
+    };
+  }
+  if (event.query.at === undefined) {
+    console.error("Could not find offset token.");
+    return {
+      statusCode: 400,
+      body: JSON.stringify('400 Bad Request (1:1)'),
+    };
+  }
+  let accessToken = event.query.at;
+  console.log(accessToken);
+  if (event.query.unique_key === undefined) {
+    console.error("Could not find unique key.");
+    return {
+      statusCode: 400,
+      body: JSON.stringify('400 Bad Request (1:2)'),
+    };
+  }
+  let uniqueKey = event.query.unique_key;
+  console.log(uniqueKey);
+
+  let statusCode = 500;
+  let body = JSON.stringify('500 Internal Server Error');
+
+  await fetch('https://api.thebase.in/1/orders/detail/' + uniqueKey, {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + accessToken
+    }
+  })
+    .then((res) => res.json())
+    .then(data => {
+      console.log(data);
+      if (data.error) throw data;
+      statusCode = 200;
+      body = data;
+    })
+    .catch(err => {
+      console.error(err);
+      statusCode = 400;
+      body = JSON.stringify('400 Bad Request (2)');
+    });
+
+  return {
+    statusCode: statusCode,
+    body: body,
+  };
+};
