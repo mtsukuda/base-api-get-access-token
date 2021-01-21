ddcafdcbb124c8d39baae16a90307a7a7c02d977
mtsukuda <mats.tsukuda@gmail.com>
2021-01-21 09:47:13 +0900

    add row: subscription_times

--- src/orders/_orders.js
+++ src/orders/_orders.js
@@ -7,6 +7,7 @@ exports.ordersPreProcessing = (data) => {
   let preProcessingData = [];
   data.orders.forEach((row) => {
     row["ordered_format"] = new Date(row.ordered * 1000).toLocaleDateString('ja-JP');
+    row["subscription_times"] = row.subscription.unique_key === null ? "" : row.subscription.repeat_number;
     preProcessingData.push(row);
   });
   data.orders = preProcessingData;
